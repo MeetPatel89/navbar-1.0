ea64047aa9a2cd65c1200512363017e6472a076f
Meet Patel <meetaiims@yahoo.com>
2021-03-17 00:37:50 -0500

    add hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
-import logo from './images/nautilus.png'
+import logo from './images/nautilus.png';
+import { useState } from 'react';
+import HamburgerMenu from 'react-hamburger-menu';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
   return (
     <>
       <header>
@@ -26,7 +35,9 @@ function App() {
           </ul>
         </nav>
         <label htmlFor='nav-toggle' className='nav-toggle-label'>
-          <span>X</span>
+          <span>
+            <HamburgerMenu isOpen={open} menuClicked={handleClick} color='white' height={20} />
+          </span>
         </label>
       </header>
     </>
